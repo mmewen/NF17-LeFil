b93b41504499c32673a2dfb7023826804398e6ef
ArthurPons <pons.arthur@gmail.com>
2015-06-04 18:48:53 +0200

    Mewen = rabat-joie

--- controleurs/accueil.php
+++ controleurs/accueil.php
@@ -2,6 +2,6 @@
 if(isset($_GET['connexion']) && $_GET['connexion']==1)
 	Messages::info('Connexion réussie');
 if(isset($_GET['deconnexion']) && $_GET['deconnexion']==1)
-	Messages::info('Déconnexion résussie');
+	Messages::info('Déconnexion réussie');
 
 include 'vues/accueil.php';
